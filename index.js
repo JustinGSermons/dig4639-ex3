cbab97d011ee6efd5584bb7e619f7eab73b9b2e8
Justin <jksermons@hotmail.com>
2024-02-05 03:04:15 -0500

    Created quote app

---
+++ index.js
@@ -0,0 +1,7 @@
+let quotes = [
+    "A dead end is just a good place to turn around. — Naomi Judd", 
+    "The difference between winning and losing is most often not quitting. — Walt Disney", 
+    "The time is always right to do what is right. — Martin Luther King Jr."
+]
+// Get a integer between 0 and 2
+console.log(quotes[Math.floor(Math.random()*3)])
